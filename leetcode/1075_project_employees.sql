2b9954f0caca61772c46ae33dce3fca46efd9258
Gulnara Azizova <78263828+gazizovaa@users.noreply.github.com>
2024-12-23 21:40:52 +0400

    added 1075_project_employees.sql

---
+++ leetcode/1075_project_employees.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below 
+SELECT p.project_id, ROUND(AVG(e.experience_years), 2) AS average_years 
+FROM Project AS p INNER JOIN Employee AS e ON p.employee_id = e.employee_id 
+GROUP BY p.project_id 
+ORDER BY p.project_id ASC;
